1e2718489d9aa2d032d71088d0add71b2776cf7f
Woongbin Kang <wbk@outlook.com>
2023-10-29 15:45:05 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# date-highlighter README
+
+## Features
+
+The extension will highlight dates in the form of "yyyy-mm-dd" and apply highlights. It's useful for organizing your TODOs in a plain text or markdown file.
+
+![Date Highlighting example](example-highlight.png)
+
+There is a very rudimentary "doneness" check to not highlight items that are done. 
+
+* `~~` immediately preceding the dates
+* Any `[x]` preceding the dates in the same line
+
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `datehighlighter.onlyHighlightGlob`: Array of of glob strings that match file paths to limit highlighting. See [glob patterns](https://code.visualstudio.com/docs/editor/glob-patterns) for reference.
+
+This extension contributes the following color settings (Place it under `workbench.colorCustomizations`):
+
+* `wbk.datehighlighter.overdueBackground`: background color for overdue dates.
+* `wbk.datehighlighter.todayBackground`: background color for today's date
+* `wbk.datehighlighter.futureBackground`:  background color for future dates
+
+## Known Issues / Requests
+
+* The highlighting is done using decorators, so there is some delay after you type the text.
+* It does not use a very performant method to match dates. Don't use it with big files.
+
+You could file requests [here](https://github.com/wbkang/vscode-ext-date-highlighter/issues)
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 0.0.1
+
+Initial release.
+
+---
+
+## Following extension guidelines
+
+Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
+
+* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
---
+++ src/extension.ts
@@ -0,0 +1,144 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    // derived example from https://github.com/microsoft/vscode-extension-samples/blob/main/decorator-sample/src/extension.ts
+    let timeout: ReturnType<typeof setTimeout> | undefined = undefined;
+
+    const overDueDateDecorationType = vscode.window.createTextEditorDecorationType({
+        backgroundColor: { id: 'wbk.datehighlighter.overdueBackground' },
+        overviewRulerColor: { id: 'wbk.datehighlighter.overdueBackground' },
+        overviewRulerLane: vscode.OverviewRulerLane.Right,
+    });
+
+    const todayDateDecorationType = vscode.window.createTextEditorDecorationType({
+        backgroundColor: { id: 'wbk.datehighlighter.todayBackground' },
+        overviewRulerColor: { id: 'wbk.datehighlighter.todayBackground' },
+        overviewRulerLane: vscode.OverviewRulerLane.Right,
+    });
+
+    const futureDateDecorationType = vscode.window.createTextEditorDecorationType({
+        backgroundColor: { id: 'wbk.datehighlighter.futureBackground' },
+        overviewRulerColor: { id: 'wbk.datehighlighter.futureBackground' },
+        overviewRulerLane: vscode.OverviewRulerLane.Right,
+    });
+    
+
+    let activeEditor = vscode.window.activeTextEditor;
+
+
+	function updateDecorations() {
+		if (!activeEditor) {
+			console.debug("Skip highlighting because there is no activeEditor");
+			return;
+		}
+		
+		const config = vscode.workspace.getConfiguration("datehighlighter");
+		let allowGlobs: Array<String> = config.get("onlyHighlightGlob") as Array<String>;
+		let anyMatch = allowGlobs.some((glob) => {
+			if (!activeEditor) {
+				return false;
+			}
+			return vscode.languages.match({pattern: glob as vscode.GlobPattern}, activeEditor.document) !== 0;
+		});
+		if (!anyMatch) {
+			console.debug("Skip highlighting because globs don't match.");
+			return;
+		}
+
+	
+		const regEx = /(\d{4})-(\d{2})-(\d{2})/g;
+		const text = activeEditor.document.getText();
+		const overdues: vscode.DecorationOptions[] = [];
+		const todays: vscode.DecorationOptions[] = [];
+        const futures: vscode.DecorationOptions[] = [];
+		let match;
+		while ((match = regEx.exec(text))) {
+			const startPos = activeEditor.document.positionAt(match.index);
+			const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+			const decoration = { 
+                range: new vscode.Range(startPos, endPos), 
+                hoverMessage: 'Number **' + match[0] + '**' 
+            };
+			
+			// super hacky markdown strike detection
+			if (match.index-2 >= 0) {
+				if (text.substring(match.index-2, match.index) === "~~") {
+					continue;
+				}
+			}
+			// super hacky markdown [x] detection
+			let lastNewlinePos = text.lastIndexOf('\n', match.index);
+			let curLine;
+			if (lastNewlinePos !== -1) {
+				curLine = text.substring(lastNewlinePos+1, match.index);
+			} else {
+				curLine = text.substring(0, match.index);
+			}
+			if (curLine.indexOf("[x]") > 0) {
+				continue;
+			}
+			
+
+            let matchedDate = new Date(parseInt(match[1]), parseInt(match[2])-1, parseInt(match[3]));
+            let now = new Date();
+            let todaysDate = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+            
+            if (matchedDate.getTime() < todaysDate.getTime()) {
+                overdues.push(decoration);
+            }
+            else if (matchedDate.getTime() === todaysDate.getTime()) {
+                todays.push(decoration);
+            } else {
+                futures.push(decoration);
+            }
+		}
+		activeEditor.setDecorations(overDueDateDecorationType, overdues);
+		activeEditor.setDecorations(todayDateDecorationType, todays);
+        activeEditor.setDecorations(futureDateDecorationType, futures);
+	}
+
+	function triggerUpdateDecorations(throttle = false) {
+		if (timeout) {
+            let validTimer:ReturnType<typeof setTimeout> = timeout;
+			clearTimeout(validTimer);
+			timeout = undefined;
+		}
+		if (throttle) {
+			timeout = setTimeout(updateDecorations, 500);
+		} else {
+			updateDecorations();
+		}
+	}
+
+	if (activeEditor) {
+		triggerUpdateDecorations();
+	}
+
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+		if (editor) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	vscode.workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+			triggerUpdateDecorations(true);
+		}
+	}, null, context.subscriptions);
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "date-highlighter" is now active!');
+
+
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
